0793c5f5d765f7a9bdff389d5c95b3f7b3342529
Derrik Petrin <derrik.petrin@pm.me>
2026-05-09 13:21:04 -0700

    demo of shared vs. unshared

--- Main.lean
+++ Main.lean
@@ -1,4 +1,37 @@
 import Hackathon
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+def sort : [Ord α] → Array α → Array α := Array.mergeSort
+
+def getLines : IO (Array String) := do
+  let stdin ← IO.getStdin
+  let mut lines : Array String := #[]
+  let mut currLine ← stdin.getLine
+  while !currLine.isEmpty do
+    -- Drop trailing newline:
+    lines := lines.push (currLine.dropRight 1)
+    currLine ← stdin.getLine
+  return lines
+
+def mainUnique : IO Unit := do
+  let lines ← getLines
+  for line in sort lines do
+    IO.println line
+
+def mainShared : IO Unit := do
+  let lines ← getLines
+  IO.println "--- Sorted lines: ---"
+  for line in sort lines do
+    IO.println line
+
+  IO.println ""
+  IO.println "--- Original data: ---"
+  for line in lines do
+    IO.println line
+
+def main (args : List String) : IO UInt32 := do
+  match args with
+  | ["--shared"] => mainShared; return 0
+  | ["--unique"] => mainUnique; return 0
+  | _ =>
+    IO.println "Expected single argument, either \"--shared\" or \"--unique\""
+    return 1
